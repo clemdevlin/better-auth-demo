16cc1747d909795bdf16e7be25c99579d2d2d558
clemdevlin <egyayemi2006@gmail.com>
2025-10-31 18:09:37 +0000

    refactor: standardize import statements and improve code formatting

--- src/app/auth/login/_components/social-auth-buttons.tsx
+++ src/app/auth/login/_components/social-auth-buttons.tsx
@@ -1,16 +1,15 @@
-"use client"
+"use client";
 
-import { BetterAuthActionButton } from "@/components/auth/better-auth-action-button"
-import { Button } from "@/components/ui/button"
-import { authClient } from "@/lib/auth/auth-client"
+import { BetterAuthActionButton } from "@/components/auth/better-auth-action-button";
+import { authClient } from "@/lib/auth/auth-client";
 import {
   SUPPORTED_OAUTH_PROVIDER_DETAILS,
   SUPPORTED_OAUTH_PROVIDERS,
-} from "@/lib/auth/o-auth-providers"
+} from "@/lib/auth/o-auth-providers";
 
 export function SocialAuthButtons() {
-  return SUPPORTED_OAUTH_PROVIDERS.map(provider => {
-    const Icon = SUPPORTED_OAUTH_PROVIDER_DETAILS[provider].Icon
+  return SUPPORTED_OAUTH_PROVIDERS.map((provider) => {
+    const Icon = SUPPORTED_OAUTH_PROVIDER_DETAILS[provider].Icon;
 
     return (
       <BetterAuthActionButton
@@ -20,12 +19,12 @@ export function SocialAuthButtons() {
           return authClient.signIn.social({
             provider,
             callbackURL: "/",
-          })
+          });
         }}
       >
         <Icon />
         {SUPPORTED_OAUTH_PROVIDER_DETAILS[provider].name}
       </BetterAuthActionButton>
-    )
-  })
+    );
+  });
 }
